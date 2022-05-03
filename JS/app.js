e086ac8d76aef2c0abd1ab2ee495e882a544ce33
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-05-03 17:06:58 +0200

    [UP]
    Mise à jour des fichiers.

--- JS/app.js
+++ JS/app.js
@@ -83,7 +83,7 @@ form.addEventListener('submit', e =>{
     if((regexNomPrenom.test(document.getElementById('name').value) != true) && (regexMessage.test(document.getElementById('message').value) != true) && (regexMessage.test(document.getElementById('subject').value) != true) && (RegexMail.test(document.getElementById('mail').value) != true)){
       span.appendChild(error)
     }
-    fetch('../PHP/index.php', {
+    fetch('./PHP/index.php', {
         method: 'POST',
         body: formData
     }).then((response) =>{
@@ -114,46 +114,63 @@ let url = 'https://mediumpostapi.herokuapp.com/?usermedium=adriensoriano';
 fetch(url)
 .then((resp) => resp.json())
 .then((data) => {
-    console.log(data.dataMedium);
-    for (let i = 0; i < 6; i++) {
-      const element = data.dataMedium[i];
+    let content = data.dataMedium;
+    for (let i = 0; i < Object.keys(data.dataMedium).length; i++) {
+      let element = content[i];
+      // création de la div.contain
       const contain = document.createElement('div');
       contain.classList.add("contain", "mt-5");
+      // création de la balise <a> avec un href qui prend en compte l'objet link de data.dataMedium et je lui rajoute l'attribut target="_blank"
       const link = document.createElement('a');
       link.href = element.link;
-      link.setAttribute("target", "_blank")
+      link.setAttribute("target", "_blank");
+      // création d'une div
       const div = document.createElement('div');
+      // création de la div qui contient tous les éléments
       const divBloc = document.createElement('div');
       divBloc.classList.add("d-flex", "flex-column", "align-items-center");
+      // création de la balise image qui contient l'image utilisée pour l'article
       const image = document.createElement('img');
       image.src=element.image;
       image.setAttribute("alt", "image de l'article de blog");
+      // création de la div qui contient la boite pour afficher la date de création de l'article
       const divDate = document.createElement('div');
       divDate.classList.add("date");
       const spanDate = document.createElement('span');
       spanDate.setAttribute("id", "date-blog");
-      let dateBlog = document.createTextNode(element.date)
+      let dateBlog = document.createTextNode(element.date);
+      // création de la div qui contient le titre et la description de l'article
       const divDescr = document.createElement('div');
       divDescr.classList.add("project", "article", "d-flex", "flex-column", "align-items-center", "p-0", "mt-2");
+      // création du titre
       const title = document.createElement('h6');
-      title.classList.add("mb-4")
+      title.classList.add("mb-4");
       let titleBlog = document.createTextNode(element.title);
+      // création de la description
       const p = document.createElement('p');
       p.classList.add("descr-projet", "text-center");
       
+      // placement de la div.contain dans la div blog
       divBlog.appendChild(contain);
+      // placement de la balise <a> dans la div.contain
       contain.appendChild(link);
+      // placement de la balise <div> dans la balise <a>
       link.appendChild(div);
+      // placement de la balise div.bloc dans la balise <div>
       div.appendChild(divBloc);
+      // placement des balises <image>, div.date et div.descr dans la balise <div>
       divBloc.appendChild(image);
       divBloc.appendChild(divDate);
+      // placement de la balise <span> contenant la date dans la balise div.date
       divDate.appendChild(spanDate);
       spanDate.appendChild(dateBlog)
       divBloc.appendChild(divDescr);
+      // placement du titre et de la description
       divDescr.appendChild(title);
       title.appendChild(titleBlog);
       divDescr.appendChild(p);
       p.innerHTML = element.description;
+      // si le texte contient plus de 200 caractères, alors il n'affiche que les 175 premiers
       if(element.description.length > 200){
         p.innerHTML = element.description.substring(0, 175);
       }
